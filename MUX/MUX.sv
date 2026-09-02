96e8cf5ee60c66034e45ff2861c54f530725d1b1
Aryan Koliar <aryankoliar30@gmail.com>
2026-09-02 04:23:37 -0500

    Fixed MUX

--- MUX/MUX.sv
+++ MUX/MUX.sv
@@ -12,8 +12,7 @@ module MUX (
         case (S)
             2'b00: Y = I[0];
             2'b01: Y = I[1];
-            2'b10: Y = I[2];
-            2'b11: Y = I[3];
+            
             default: Y = 0;
         endcase
     end
